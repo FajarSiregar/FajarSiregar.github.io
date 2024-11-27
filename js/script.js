7225674fccac854e30bb4fed294f934ff99d3adc
unknown <fahdillahfajar@gmail.com>
2024-11-27 22:19:25 +0700

    Penambahan Element dan Fitur

--- js/script.js
+++ js/script.js
@@ -1,17 +1,74 @@
-// Toggle Class Active
+// Toggle Class Active Untuk Hamburger Menu
 
 const navbarNav = document.querySelector('.navbar-nav');
 
-// Perintah Ketika Hambuerger Menu di Klik
+// Perintah Ketika Hamburger Menu di Klik
 document.querySelector('#hamburger-menu').onclick = () => {
     navbarNav.classList.toggle('active');
 };
 
-// Perintah Untuk Klik dimana Saja Untuk Menghilangkan Nav
-const hamburger = document.querySelector("#hamburger-menu");
+// Defenisi Toggle Class Active Untuk Search Form, Search Box dan Shopping Cart
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+const shoppingCart = document.querySelector(".shopping-cart");
+
+// Toggle Class Active Untuk Search Box
+document.querySelector("#search-button").onclick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+};
+
+// Toggle Class Active Untuk Shopping Cart
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+    shoppingCart.classList.toggle("active");
+    e.preventDefault();
+}
+
+// Perintah Untuk Klik dimana Saja Untuk Menghilangkan Element
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
+const scb = document.querySelector("#shopping-cart-button");
+
 
 document.addEventListener("click", function (e) {
-    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    //Element Navbar
+    if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove("active");
     }
+
+    // Element Search Box
+    if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove("active");
+    }
+
+    // Element Shopping Cart
+    if (!scb.contains(e.target) && !shoppingCart.contains(e.target)) {
+        shoppingCart.classList.remove("active");
+    }
 });
+
+// Modal Box
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+
+itemDetailButtons.forEach((btn) => {
+    btn.onclick = (e) => {
+        itemDetailModal.style.display = "flex";
+        e.preventDefault();
+    };
+})
+
+
+// Klik Tombol Close Untuk Keluar Modal
+document.querySelector(".modal .close-icon").onclick = (e) => {
+    itemDetailModal.style.display = "none";
+    e.preventDefault();
+};
+
+// Perintah Klik Diluar Element Agar Close dari Modal Box
+window.onclick = (e) => {
+    if (e.target === itemDetailModal) {
+        itemDetailModal.style.display = "none";
+    }
+};
